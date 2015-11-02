3246192e71ad0e8a8b6b376bcd26b804035ac05e
Yaroslav F <yarfilyk@gmail.com>
2015-11-02 15:49:40 +0000

    Update settings_helper.rb

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -2,22 +2,10 @@ module SettingsHelper
   def settings_field_options(settings_name, value)
     default_value = ActiveadminSettingsCached.defaults[settings_name]
 
-    input_opts = if default_value.is_a?(Array)
-                  {
-                    collection: default_value,
-                    selected: value,
-                  }
-                elsif [true, false].include?(default_value)
-                  {
+    input_opts = {
                     checked: value
                   }
-                else
-                  {
-                    input_html: {value: value, placeholder: default_value},
-                  }
-                end
+                
 
-    {as: ActiveadminSettingsCached.config.display[settings_name], label: false}
-      .merge!(input_opts)
   end
 end
